ddf5ecf7fcbe16b599bed312dc6ff4167df9d5c5
ze00 <zerozakiGeek@gmail.com>
2018-01-17 20:29:45 +0800

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 version=$(getprop ro.build.version.release)
-if [ "${version%.*}" -gt 6 ];
+if [ "${version%.*}" -gt 6 ];then
   wget 'https://raw.githubusercontent.com/ze00/pvz/master/release/cheater_nought' -O cheater
 else
   wget 'https://raw.githubusercontent.com/ze00/pvz/master/release/cheater_marshmallow' -O cheater
