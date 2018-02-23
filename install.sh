8ab068851e423a1199b9e73e69f96a7fbc799e47
ze00 <zerozakiGeek@gmail.com>
2018-02-23 11:18:33 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@ version=$(/system/bin/getprop ro.build.version.release)
 
 if [ "${version%%.*}" -gt "6" ];then
   if [ -d "/system/lib64" ];then
-    wget 'https://raw.githubusercontent.com/ze00/pvz/master/release/arm64/cheater_nought' -O cheater
+    curl 'https://raw.githubusercontent.com/ze00/pvz/master/release/arm64/cheater_nought' > cheater
   else
-    wget 'https://raw.githubusercontent.com/ze00/pvz/master/release/arm/cheater_nought' -O cheater
+    curl 'https://raw.githubusercontent.com/ze00/pvz/master/release/arm/cheater_nought' > cheater
   fi
 else
   if [ -d "/system/lib64" ];then
-    wget 'https://raw.githubusercontent.com/ze00/pvz/master/release/arm64/cheater_marshmallow' -O cheater
+    curl 'https://raw.githubusercontent.com/ze00/pvz/master/release/arm64/cheater_marshmallow' > cheater
   else
-    wget 'https://raw.githubusercontent.com/ze00/pvz/master/release/arm/cheater_marshmallow' -O cheater
+    curl 'https://raw.githubusercontent.com/ze00/pvz/master/release/arm/cheater_marshmallow' > cheater
   fi
 fi
 chmod a+x cheater
