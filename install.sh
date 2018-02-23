addc08e249755d71c5728f8961db4c4ca7aded39
ze00 <zerozakiGeek@gmail.com>
2018-02-23 17:47:37 +0800

    What:   * update install.sh Why:   * 代码重复 How:   *

--- install.sh
+++ install.sh
@@ -1,17 +1,15 @@
 #!/system/bin/sh
 version=$(/system/bin/getprop ro.build.version.release)
 
-if [ "${version%%.*}" -gt "6" ];then
-  if [ -d "/system/lib64" ];then
-    curl 'https://raw.githubusercontent.com/ze00/pvz/master/release/arm64/cheater_nought' > cheater
-  else
-    curl 'https://raw.githubusercontent.com/ze00/pvz/master/release/arm/cheater_nought' > cheater
-  fi
+if [ -d "/system/lib64" ];then
+  arch="arm64"
 else
-  if [ -d "/system/lib64" ];then
-    curl 'https://raw.githubusercontent.com/ze00/pvz/master/release/arm64/cheater_marshmallow' > cheater
-  else
-    curl 'https://raw.githubusercontent.com/ze00/pvz/master/release/arm/cheater_marshmallow' > cheater
-  fi
+  arch="arm"
 fi
+if [ "${version%%.*}" -gt "6" ];then
+  version="nought"
+else
+  version="marshmallow"
+fi
+curl "https://raw.githubusercontent.com/ze00/pvz/master/release/${arch}/cheater_${version}" > cheater
 chmod a+x cheater
